e860c0693d1d9c858d19a19ccc3171b68056a858
Infayou Digital <159997668+anurag8824@users.noreply.github.com>
2025-04-23 10:25:33 +0530

    Update connectDB.js

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -16,9 +16,9 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',  // XAMPP's default root password is blank
+    host: 'localhost', // or '127.0.0.1' since it's on the same EC2 server
+    user: 'gameuser',
+    password: 'Avatior@123',
     database: 'gameavitor',
     port: 3306
 });
