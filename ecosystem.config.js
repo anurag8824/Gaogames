e6b29aa658f61caa38c3e3b98c242b1160db6c2a
Infayou Digital <159997668+anurag8824@users.noreply.github.com>
2025-04-29 11:51:03 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: "./src/server.js",   // ✅ Notice "./src/server.js" because server.js is inside src
       exec_mode: "cluster",
       instances: "max",
+      node_args: '--require sticky-cluster',
       autorestart: true,
       max_memory_restart: "500M",
       env_file: ".env"
